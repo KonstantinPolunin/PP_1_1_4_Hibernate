723e811f80ab17c54d92d9121208dc8185770066
Константин Полунин <tinyyy052@gmail.com>
2023-01-06 12:18:03 +0300

    fix Main, User, UserDaoJDBCImpl, UserServiceImpl, Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+
+    private static final String URL = "jdbc:mysql://localhost:3306/firstdb";
+    private static final String userName = "root";
+    private static final String password = "AAaa1#85";
+
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, userName, password);
+            System.out.println("Connection is OK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
